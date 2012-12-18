061f629d7a140f14aae6f95d4b7171c192b078c9
Greg Novak <greg.novak@gmail.com>
2012-12-18 11:46:58 -0500

    Code to do pointwise averages of numpy arrays

--- structure.py
+++ structure.py
@@ -2016,6 +2016,22 @@ def popKeys(d, *names):
     args"""
     return dict([(k, d.pop(k)) for k in names if k in d])
 
+def ave(a, n=1, axis=-1):
+    if n==0: return np.array(a)
+    if n < 0: raise ValueError, 'order must be non-negative but got ' + repr(n)
+    a = np.asarray(a)
+    nd = len(a.shape)
+    slice1 = [slice(None)]*nd
+    slice2 = [slice(None)]*nd
+    slice1[axis] = slice(1, None)
+    slice2[axis] = slice(None, -1)
+    slice1 = tuple(slice1)
+    slice2 = tuple(slice2)
+    if n > 1:
+        return ave(0.5*(a[slice1]+a[slice2]), n-1, axis=axis)
+    else:
+        return 0.5*(a[slice1]+a[slice2])
+
 ##############################
 ### Routines to read and write fortran files.  
 ###
